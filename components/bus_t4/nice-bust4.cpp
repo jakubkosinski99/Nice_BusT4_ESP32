d5f8311cc48769dad81289946b201d16e6eed7a8
Jakub Kosiński <jakub.kosinski.99@gmail.com>
2025-06-03 20:04:50 +0200

    Fix uart num

--- components/bus_t4/nice-bust4.cpp
+++ components/bus_t4/nice-bust4.cpp
@@ -65,22 +65,7 @@ void NiceBusT4::control(const CoverCall &call) {
 }
 
 void NiceBusT4::setup() {
-
-
- // _uart =  uart_init(_UART_NO, BAUD_WORK, SERIAL_8N1, SERIAL_6E2, TX_P, 256, false); //for ESP8266
   _uart =  uartBegin(_UART_NO, BAUD_WORK, SERIAL_8N1, RX_PIN, TX_PIN, 256, 256, false, 112); //for WT32
-  // who's online?
-//  this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
-
-  // ESP_LOGD("setup", "Wywołanie setup()");
-  // if (this->pause_time_sensor == nullptr) {
-    // // Użycie operatora & do uzyskania wskaźnika do id(pause_time_sensor)
-    // this->pause_time_sensor = &id(pause_time_sensor);
-    // ESP_LOGD("setup", "pause_time_sensor został przypisany w setup: %p", this->pause_time_sensor);
-  // } else {
-    // ESP_LOGW("setup", "pause_time_sensor już został przypisany");
-  // }
-
 }
 
 void NiceBusT4::loop() {
--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -67,7 +67,7 @@ using namespace esphome::cover;
 //using esp8266::timeoutTemplate::oneShotMs;
 
 
-static const int _UART_NO=1; /* uart number */
+static const int _UART_NO=2; /* uart number */
 //static const int _UART_NO=UART_NUM_2;
 static const int TX_PIN = 17;           /* pin Tx */
 static const int RX_PIN = 5;           /* pin Rx */
